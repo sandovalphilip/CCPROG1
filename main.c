dade936441facf8ddc92c3b98ec8332a9d57c096
sandovalphilip <philip_sandoval@dlsu.edu.ph>
2024-05-17 19:21:36 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main()
-{
-  
-  return 0;
+int main() {
+  char name; 
+  char product;
+  int price;
+  int quantity;
+  int x;
+
+  printf("Hello, what is your name?: ");
+  scanf("%c", &name);
+  printf("Hello, what is the product?: ");
+  scanf("%c", &product);
+  printf("Hello, what is the price?: ");
+  scanf("%d", &price);
+  printf("Hello, what is the quantity?: ");
+  scanf("%d", &quantity);  
+
+  for (x = 1; x <= quantity; x++) {
+	  printf("%c, %d", product, price);
+      }
+
+return 0;
 }
